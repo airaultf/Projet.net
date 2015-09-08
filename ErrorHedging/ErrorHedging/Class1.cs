86094f9e4f827b7bbb9f61b03dba59ecd08dfb97
alexbronner <alexandre.bronner@ensimag.grenoble-inp.fr>
2015-09-08 11:35:08 +0200

    Revert "Revert "Ajout projet""
    This reverts commit 3e4b248e3d6c768cfe06a3ec69da8faa888ca8df.

---
+++ ErrorHedging/ErrorHedging/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using
+
+namespace ErrorHedging
+{
+    class HedgingPortfolio
+    {
+        // [DllImport('wre-ensimag-c-4.1.dll')]
+        // Contient un dictionnaire d'action avec les delta correspondant
+        Dictionary<PricingLibrary.FinancialProducts.Share, double> StockDelta;
+        // Type de produit à couvrir : 0 vanilla Call, 1 basket option, 2 Option
+
+        double[] freeRate;
+
+
+        // On initialise le portefeuille de couverture avec un Vanilla Call
+        public HedgingPortfolio(PricingLibrary.FinancialProducts.VanillaCall Call)
+        {
+
+            int typeProduct = 0;
+            double[,] freeRate = new double[0, 0];
+
+            Dictionary<PricingLibrary.FinancialProducts.Share, double> StockDelta = new Dictionary<PricingLibrary.FinancialProducts.Share, double>();
+            StockDelta.Add(Call.UnderlyingShare, 0);
+        }
+
+        public double updateHedgeRatioVanillaCall(System.DateTime date, double spot, PricingLibrary.FinancialProducts.VanillaCall Call, double vol)
+        {
+
+            PricingLibrary.Computations.Pricer price = new PricingLibrary.Computations.Pricer();
+            PricingLibrary.Computations.PricingResults result = price.PriceCall(Call, date, 365, spot, vol);
+
+
+
+            return 0.0;
+        }
+
+
+
+    }
+}
