df60bc09dc585def11906540b8f3bd1817a063f6
Pierre <psabouret9@gmail.com>
2015-09-09 15:59:44 +0200

    volatility presque finie

--- ErrorHedging/HedgingTest/testResults.cs
+++ ErrorHedging/HedgingTest/testResults.cs
@@ -22,14 +22,14 @@ namespace HedgingTest
             ErrorHedging.HedgingPortfolioVanillaCall couvPort = new ErrorHedging.HedgingPortfolioVanillaCall(Call, date, 50.0, 0.2);
             Console.WriteLine("Valeur initiale ", couvPort.portfolioValue);
             // updatePortfolioValue(double spot, System.DateTime date, double volatility)
-            DateTime date1 = new DateTime(2014, 6, 1, 0, 0, 0);
+            DateTime date1 = new DateTime(2012, 6, 1, 0, 0, 0);
 
-            Results myResults = new Results(Call, date1, date, 10, true);
+            Results myResults = new Results(Call, date1, date, 30, true);
 
-            for (DateTime d = date1.AddDays(12); d < date; d=d.AddDays(1))
+            for (DateTime d = date1.AddDays(30); d < date; d=d.AddDays(1))
             {
-                Console.WriteLine("Spot price: " + myResults.getSpotPrice(d));
-                //Console.WriteLine("Volatility: " + myResults.getVolatility(d));
+                //Console.WriteLine("Spot price: " + myResults.getSpotPrice(d));
+                Console.WriteLine("Volatility: " + myResults.getVolatility(d));
             }
         }
     }
